7165eef26c139c76485dc4371ded123ee9652223
Sardoroff <eshpolatovsardor18@gmail.com>
2024-11-06 17:32:27 +0500

    pinia

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -16,28 +16,28 @@ export const useCounterStore = defineStore('counter', () => {
       id: 1,
       title: 'Magazin 1',
       url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmSHiu9PwPaAh_lZbNiQaaqaJ4jzy9LsW5ng&s',
-      description: 'High-quality product 1.',
+      description: '1 High-quality product.',
       price: 100
     },
     {
       id: 2,
       title: 'Magazin 2',
       url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIfbDp22IASwnbQr_ehSFEKLQVETHTsy4tvg&s',
-      description: 'High-quality product 2.',
+      description: '2 High-quality product.',
       price: 150
     },
     {
       id: 3,
       title: 'Magazin 3',
       url: 'https://frankfurt.apollo.olxcdn.com/v1/files/7njw7t3ot2r02-UZ/image;s=1280x996',
-      description: 'High-quality product 3.',
+      description: '3 High-quality product.',
       price: 200
     },
     {
       id: 4,
       title: 'Magazin 4',
       url: 'https://frankfurt.apollo.olxcdn.com/v1/files/xofinem8ns3p3-UZ/image;s=960x960',
-      description: 'High-quality product 4.',
+      description: '4 High-quality product.',
       price: 250
     }
   ]);
@@ -53,9 +53,9 @@ export const useCounterStore = defineStore('counter', () => {
     if (url) {
       const newPost: Post = {
         id: posts.value.length + 1,
-        title: `Magazin ${posts.value.length + 1}`,
+        title: ` ${posts.value.length + 1} Magazin`,
         url,
-        description: `High-quality product ${posts.value.length + 1}.`,
+        description: `${posts.value.length + 1} High-quality product.`,
         price: 100 + posts.value.length * 50
       };
       posts.value.push(newPost);
